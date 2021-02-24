7f189369477f5245b341ae01b482fe3ee09c0a61
Woojoong Kim <woojoong@opennetworking.org>
2021-02-23 19:39:32 -0800

    Add Makefile and main go file

---
+++ cmd/onos-pci/onos-pci.go
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+package main
+
+import (
+	"fmt"
+	"github.com/onosproject/onos-lib-go/pkg/logging"
+)
+
+var log = logging.GetLogger("main")
+
+func main() {
+	fmt.Print("Start")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/onosproject/onos-pci
+
+go 1.14
+
+require github.com/onosproject/onos-lib-go v0.7.3
