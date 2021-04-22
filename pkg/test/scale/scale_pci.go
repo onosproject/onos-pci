0e1177ace861686a327ee34a2a98585f61897fae
Woojoong Kim <woojoong@opennetworking.org>
2021-04-21 18:54:38 -0700

    add tls keys

--- pkg/test/scale/scale_pci.go
+++ pkg/test/scale/scale_pci.go
@@ -5,9 +5,11 @@
 package scale
 
 import (
+	"github.com/onosproject/onos-lib-go/pkg/certs"
 	"github.com/onosproject/onos-pci/pkg/manager"
 	"testing"
 	"time"
+	"log"
 )
 
 func (s *TestSuite) TestScalePci(t *testing.T) {
@@ -16,15 +18,31 @@ func (s *TestSuite) TestScalePci(t *testing.T) {
 	e2subEndpoint := "onos-e2sub:5150"
 
 	cfg := manager.Config{
+		CAPath:        "certs/tls.cacrt",
+		KeyPath:        "certs/tls.key",
+		CertPath:        "certs/tls.crt",
 		E2tEndpoint:   e2tEndpoint,
 		E2SubEndpoint: e2subEndpoint,
 		GRPCPort:      5150,
 		RicActionID:   int32(10),
 	}
 
+	_, err := certs.HandleCertPaths(cfg.CAPath, cfg.KeyPath, cfg.CertPath, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	time.Sleep(10 * time.Second)
-	ready := make(chan bool)
+	//ready := make(chan bool)
 	mgr := manager.NewManager(cfg)
+
 	mgr.Run()
-	<- ready
+	for {
+		time.Sleep(1 * time.Second)
+		mgr.Mons.PciMonitorMutex.RLock()
+		log.Printf("mgr.Mons.PciMonitor: %v", mgr.Mons.PciMonitor)
+		mgr.Mons.PciMonitorMutex.RUnlock()
+
+	}
+	//<- ready
 }
