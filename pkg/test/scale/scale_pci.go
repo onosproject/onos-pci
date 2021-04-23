36f0f5775531e0b5b4a00bf6285ef315c27f396b
Woojoong Kim <woojoong@opennetworking.org>
2021-04-22 17:04:40 -0700

    Add assert blocks to decide if test passes or not

--- pkg/test/scale/scale_pci.go
+++ pkg/test/scale/scale_pci.go
@@ -7,6 +7,8 @@ package scale
 import (
 	"github.com/onosproject/onos-lib-go/pkg/certs"
 	"github.com/onosproject/onos-pci/pkg/manager"
+	"github.com/onosproject/onos-pci/pkg/store"
+	"github.com/stretchr/testify/assert"
 	"log"
 	"testing"
 	"time"
@@ -29,24 +31,52 @@ func (s *TestSuite) TestScalePci(t *testing.T) {
 	}
 
 	_, err := certs.HandleCertPaths(cfg.CAPath, cfg.KeyPath, cfg.CertPath, true)
-	if err != nil {
-		log.Fatal(err)
-	}
+	assert.NoError(t, err)
 
 	time.Sleep(10 * time.Second)
-	//ready := make(chan bool)
+	resultCh := make(chan map[string]*store.PciStat)
+	timer := make(chan bool)
 	mgr := manager.NewManager(cfg)
-
 	mgr.Run()
-	for {
-		time.Sleep(1 * time.Second)
-		mgr.Mons.PciMonitorMutex.RLock()
-		log.Printf("mgr.Mons.PciMonitor (length: %d): %v", len(mgr.Mons.PciMonitor), mgr.Mons.PciMonitor)
-		for k, v := range mgr.Mons.PciMonitor {
-			log.Printf("ID %s: %d\n", k, v)
-		}
-		mgr.Mons.PciMonitorMutex.RUnlock()
 
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			resultCh <- mgr.Mons.PciMonitor
+		}
+	}()
+
+	// timer
+	go func() {
+		time.Sleep(60 * time.Second)
+		timer <- true
+	}()
+
+
+	for {
+		numConflicts := int32(0)
+		select {
+		case <- timer:
+			mgr.Mons.PciMonitorMutex.RLock()
+			assert.GreaterOrEqual(t, 1, mgr.Mons.PciMonitor["343332707642115"])
+			mgr.Mons.PciMonitorMutex.RUnlock()
+		case st := <- resultCh:
+			mgr.Mons.PciMonitorMutex.RLock()
+			log.Printf("num conflicts for %s is %d", "343332707642115", st["343332707642115"].NumConflicts)
+			if st["343332707642115"].NumConflicts >= 1 {
+				numConflicts = st["343332707642115"].NumConflicts
+				break
+			}
+			log.Printf("num conflicts for %s is %d", "343332707642118", st["343332707642118"].NumConflicts)
+			if st["343332707642118"].NumConflicts >= 1 {
+				numConflicts = st["343332707642118"].NumConflicts
+				break
+			}
+			mgr.Mons.PciMonitorMutex.RUnlock()
+
+		}
+		if numConflicts >= 1 {
+			break
+		}
 	}
-	//<- ready
 }
