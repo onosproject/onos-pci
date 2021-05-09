b66ae01b25ba2ee99a7bc7c7e0c751a7c7faa8bc
Woojoong Kim <woojoong.m.kim@gmail.com>
2021-05-09 16:49:15 -0700

    Fix PCI integration test - a three-cell case (#25)

--- pkg/test/pci/suite.go
+++ pkg/test/pci/suite.go
@@ -5,6 +5,7 @@
 package pci
 
 import (
+	"github.com/onosproject/helmit/pkg/helm"
 	"github.com/onosproject/helmit/pkg/input"
 	"github.com/onosproject/helmit/pkg/test"
 	"github.com/onosproject/onos-pci/pkg/test/utils"
@@ -12,16 +13,23 @@ import (
 
 // TestSuite is the primary onos-pci test suite
 type TestSuite struct {
+	sdran *helm.HelmRelease
 	test.Suite
 }
 
 // SetupTestSuite sets up the onos-pci test suite
 func (s *TestSuite) SetupTestSuite(c *input.Context) error {
 	sdran, err := utils.CreateSdranRelease(c)
+	s.sdran = sdran
 	sdran.Set("ran-simulator.pci.metricName", "three-cell-metrics").
 		Set("ran-simulator.pci.modelName", "three-cell-model")
 	if err != nil {
 		return err
 	}
 	return sdran.Install(true)
+}
+
+// TearDownTestSuite uninstalls helm chart released
+func (s *TestSuite) TearDownTestSuite() error {
+	return s.sdran.Uninstall()
 }
