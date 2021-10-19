215285f6da355efef5aba3c3d2e409fe5800e9b4
Thomas Vachuska <tom@opennetworking.org>
2021-10-19 12:55:38 -0700

    Added teardown of proxy

--- test/scale/suite.go
+++ test/scale/suite.go
@@ -56,5 +56,6 @@ func (s *TestSuite) SetupTestSuite(c *input.Context) error {
 
 // TearDownTestSuite uninstalls helm chart released
 func (s *TestSuite) TearDownTestSuite() error {
+	testutils.StopTestProxy()
 	return s.sdran.Uninstall()
 }
